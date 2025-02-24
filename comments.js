8e5894dd895d9af79ca54cdf2aa98596ae3fe49d
Bhupendra Verma <bhupendra.verma@dotsquares.com>
2025-02-24 20:08:35 +0530

    updated suggestion

--- comments.js
+++ comments.js
@@ -1,13 +1,20 @@
-// create a web server with express
-const express = require("express");
-const app = express();
+// create a web server that listens to the port 3000
+// and serves a file called comments.js
+// when someone visits http://localhost:3000/comments.js
+// send them the file comments.js
 
-// add a route to the web server
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function (req, res) {
+  if (req.url === "/comments.js") {
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+    fs.createReadStream(__dirname + "/comments.js").pipe(res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
-// start the server
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
-});
+server.listen(3000);
+console.log("Server listening on port 3000");
