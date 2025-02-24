5aa0cebdcc88886226da2795049eb9878e741cbe
Bhupendra Verma <bhupendra.verma@dotsquares.com>
2025-02-24 20:18:34 +0530

    added new suggestion

--- comments.js
+++ comments.js
@@ -1,20 +1,35 @@
-// create a web server that listens to the port 3000
-// and serves a file called comments.js
-// when someone visits http://localhost:3000/comments.js
-// send them the file comments.js
+// create a web server with node.js
 
 var http = require("http");
 var fs = require("fs");
+var qs = require("querystring");
+var url = require("url");
 
 var server = http.createServer(function (req, res) {
-  if (req.url === "/comments.js") {
-    res.writeHead(200, { "Content-Type": "text/javascript" });
-    fs.createReadStream(__dirname + "/comments.js").pipe(res);
+  if (req.method === "POST") {
+    var body = "";
+    req.on("data", function (data) {
+      body += data;
+    });
+
+    req.on("end", function () {
+      var POST = qs.parse(body);
+      fs.readFile("comments.json", function (err, comments) {
+        comments = JSON.parse(comments);
+        comments.push(POST);
+        fs.writeFile("comments.json", JSON.stringify(comments), function (err) {
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end("Thanks for the comment!");
+        });
+      });
+    });
   } else {
-    res.writeHead(404);
-    res.end();
+    fs.readFile("comments.json", function (err, comments) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(comments);
+    });
   }
 });
 
 server.listen(3000);
-console.log("Server listening on port 3000");
+console.log("Server running on port 3000");
